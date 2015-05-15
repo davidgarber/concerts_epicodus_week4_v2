3be850f100d124be1abf406b58e649efec1000e7
David Garber <davidgarber@gmail.com>
2015-05-15 09:27:56 -0700

    lists bands a venue has hosted.

--- spec/venue_spec.rb
+++ spec/venue_spec.rb
@@ -1,1 +1,11 @@
 require('spec_helper')
+
+describe(Venue) do
+  describe('#bands') do
+    it("lists bands a venue has hosted") do
+      test_band1 = Band.create({:name => "Jay-Z"})
+      test_band2 = Band.create({:name => "Beyonce"})
+      test_venue = Venue.new({:name => "Roseland", :band_ids => [test_band1.id(), test_band2.id()]})
+    end
+  end
+end
