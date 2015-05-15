5180081fff38383ae1357ad66b256c1ba91c1281
David Garber <davidgarber@gmail.com>
2015-05-15 09:20:05 -0700

    list the venue a band has played.

--- spec/band_spec.rb
+++ spec/band_spec.rb
@@ -1,1 +1,11 @@
 require('spec_helper')
+
+describe(Band) do
+  describe('#venues') do
+    it("lists the venues a band has played") do
+      test_venue1 = Venue.create({:name => "Roseland"})
+      test_venue2 = Venue.create({:name => "Crystal Ballroom"})
+      test_band = Band.new({:name => "Jay-Z", :venue_ids => [test_venue1.id(), test_venue2.id()]})
+    end
+  end
+end
